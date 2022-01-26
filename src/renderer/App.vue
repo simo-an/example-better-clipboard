878e2129375bd9b67182920a5492435764e3d6ba
simu <seimor@126.com>
2022-01-26 08:59:57 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <button class="alt" @click="open()">查看剪贴板内容</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { betterClipboard } from 'better-clipboard'
+
+// const betterClipboard = require('better-clipboard').betterClipboard
+
+export default {
+  name: 'example-better-clipboard',
+  methods: {
+    open () {
+      console.warn(betterClipboard.readFilePathList())
+    }
+  }
+}
+</script>
+
+<style>
+  /* CSS */
+</style>
